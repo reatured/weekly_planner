0ad076fc39f35bdf2994804075ed69afb93ddb85
Lingyi Zhou <61943376+reatured@users.noreply.github.com>
2025-01-20 20:08:49 -0800

    Mid part layout

--- src/App.js
+++ src/App.js
@@ -21,18 +21,30 @@ function App() {
           <p>{date}</p>
         </div>
         
-        <div className="Weeks" style={{ width: '100px'}}>
+        <div className="Weeks" style={{ width: '100px' }}>
           <p style={{  textAlign: 'right' }}>{week}</p>
         </div>
       </div>
       
       {/* Middle Block */}
-      <div className="mid-block">
-        {/* Content for the middle block */}
+      <div className="mid-block" style={{backgroundColor: '#B38787FF'}}>
+        {/* Duplicating the date-block 7 times */}
+        {Array.from({ length: 7 }).map((_, index) => (
+          <div className="date-block" key={index}>
+            <div className="date-block-left" >
+             <p className="date-block-date">{index + 20}</p>
+             <p className="date-block-day" style={{margin: 0}}>Mon</p>
+              
+            </div>
+            <div className="date-block-right" style={{width: 'calc(100% - 106px)'}}>
+              
+            </div>
+          </div>
+        ))}
       </div>
       
       {/* Bottom Block */}
-      <div className="bottom-block">
+      <div className="bottom-block" style={{backgroundColor: '#000000FF'}}>
         {/* Content for the bottom block */}
       </div>
     </div>
